fa1f665d8e6a8df20caf27498954fe35e6abe7e9
somsaks <somsaks@9156dc64-4839-0410-bff5-3164207c4f9f>
2007-12-04 10:41:18 +0000

    Add verbose

--- trunk/rocks_solid/app.py
+++ trunk/rocks_solid/app.py
@@ -111,37 +111,9 @@ def run_cluster_clean_ps() :
 
     config = config_read()
     try :
-        ##
-        ## no need for all this anymore. cleanipcs and term-sge can be used safely
-        ##
-        #scheduler_mod = module_factory('rocks_solid.scheduler.%s' % config.scheduler)
-        #scheduler = scheduler_mod.Scheduler()
-        #online_hosts, offline_hosts = scheduler.hosts()
-        # build dictionary of online_hosts
-        #online_hosts_dict = {}
-        #for host in online_hosts :
-        #    online_hosts_dict[host.name] = 1
-        #queues = scheduler.queues()
-        #for q in queues :
-        #    for host in q.online_hosts :
-        #        if host.slot_used > 0:
-        #            try :
-        #                del online_hosts_dict[host.name]
-        #            except :
-        #                pass
-        # clean SGE zombie first
         # use tentakel because we want to run it only on compute nodes
         os.system('tentakel -g compute node-term-%s-zombie' % config.scheduler)
-        #fd, path = tempfile.mkstemp()
-        #tmpfile = os.fdopen(fd)
-        #for host in online_hosts_dict.iterkeys() :
-        #    tmpfile.write(host + '\n')
-        #tmpfile.close()
-        # use cluster-fork because tentakel don't accept external machine file
-        #os.system('cluster-fork --pe-hostfile %s --bg node-term-user-ps' % path)
-        #os.system('cluster-fork --pe-hostfile %s --bg node-cleanipcs' % path)
         os.system('tentakel -g compute node-cleanipcs')
-        #os.remove(path)
     except ImportError :
         parser.error('Unknown scheduler setting: %s' % config.scheduler)
         sys.exit(1)
@@ -306,6 +278,8 @@ def run_node_envcheck() :
 
     options, args = parser.parse_args()
     config = config_read()
+    if options.verbose :
+        setattr(config, 'verbose', True)
     try :
         log = config.env_log
     except :
