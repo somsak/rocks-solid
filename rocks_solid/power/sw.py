c6c2b32ed12c9b1aa228aa7265f9bae1567e1218
somsaks <somsaks@9156dc64-4839-0410-bff5-3164207c4f9f>
2007-12-07 05:56:51 +0000

    Add more verbose output

--- rocks_solid/power/sw.py
+++ rocks_solid/power/sw.py
@@ -11,7 +11,7 @@ class SWPower(BasePower) :
     This driver basically do not support power on
     '''
     def ssh_shutdown(self, host) :
-        cmdline = 'ssh %s %s "%s"' % (self.config.ssh_arg, host, self.config.ssh_shutdown_cmd)
+        cmdline = 'ssh %s %s "%s" && echo -n shutdown %s' % (self.config.ssh_arg, host, self.config.ssh_shutdown_cmd, host)
         cmd = popen2.Popen3(cmdline, capturestderr = True)
         output = cmd.fromchild.read()
         error = cmd.childerr.read()
@@ -22,7 +22,7 @@ class SWPower(BasePower) :
         self.launcher.launch(host_list, self.ssh_shutdown)
 
     def ssh_reboot(self, host) :
-        cmdline = 'ssh %s %s "%s"' % (self.config.ssh_arg, host, self.config.ssh_reboot_cmd)
+        cmdline = 'ssh %s %s "%s" && echo -n reboot %s' % (self.config.ssh_arg, host, self.config.ssh_reboot_cmd, host)
         cmd = popen2.Popen3(cmdline, capturestderr = True)
         output = cmd.fromchild.read()
         error = cmd.childerr.read()
@@ -33,3 +33,10 @@ class SWPower(BasePower) :
         self.launcher.launch(host_list, self.ssh_reboot)
 
 Power = SWPower
+
+if __name__ == '__main__' :
+    from rocks_solid import config_read
+
+    config = config_read()
+    power = Power(config)
+    power.off(['compute-0-0.local'])
