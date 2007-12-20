e228e783539752dd3fd4be932792407399e1cee0
somsaks <somsaks@9156dc64-4839-0410-bff5-3164207c4f9f>
2007-12-20 11:33:11 +0000

    Increase version (Added support for database)

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ import %s
 
 setup(
     name = "rocks-solid",
-    version = "0.2",
+    version = "0.3",
 #    packages = find_packages(),
     packages = list_my_packages(),
 #    scripts = glob.glob('scripts/*-*'),
