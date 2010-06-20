54a04522a0783552e872d6eae2fe1411ef0df700
somsaks <somsaks@9156dc64-4839-0410-bff5-3164207c4f9f>
2010-06-20 09:32:27 +0000

    Interface for dbreport

--- trunk/rocks_solid/cluster/__init__.py
+++ trunk/rocks_solid/cluster/__init__.py
@@ -12,7 +12,7 @@ class ClusterIntf(object) :
         '''
         pass
 
-    def list_ethers(self, **kwargs) :
+    def list_network(self, **kwargs) :
         '''
         Get mapping between host and ethernet address (MAC Address0
 
@@ -62,9 +62,53 @@ class Rocks(ClusterIntf) :
 
         return host_list
         
-    def list_ethers(self, **kwargs) :
+    def list_network(self, **kwargs) :
+        # sanity check
+        if not self.rocks_cmd and not self.dbreport_cmd :
+            return []
+
+        interface_map = {}
+        if self.rocks_cmd :
+            cmd = subprocess.Popen([self.rocks_cmd, "list", "host", "interface"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+        elif self.dbreport_cmd :
+            cmd = subprocess.Popen([self.dbreport_cmd, "ethers"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+
+        first_line = True
+        while True :
+            line = cmd.stdout.readline()
+            if not line:
+                break
+            if self.rocks_cmd and first_line :
+                first_line = False
+                continue
+            if self.rocks_cmd :
+                fields = line.strip().split()
+                if fields and len(fields) >= 1 :
+                    # stripped out trailing ':'
+                    host = fields[0][:-1]
+                    iface = fields[2]
+                    mac = fields[3]
+                    ip = fields[4]
+                    field_to_add = {'name':iface, 'mac': mac, 'ip':ip}
+                    if interface_map.has_key(host) :
+                        interface_map[host].append(field_to_add)
+                    else :
+                        interface_map[host] = [field_to_add]
+            elif self.dbreport_cmd :
+                mac, host = line.strip().split()
+                if interface_map.has_key(host) :
+                    interface_map[host].append({'mac': mac})
+                else :
+                    interface_map[host] = [{'mac': mac}]
+                
+        cmd.stdout.close()
+        retcode = cmd.wait()
+
+        return interface_map
+ 
         pass
 
 if __name__ == '__main__' :
     rocks = Rocks()
     print rocks.list_host()
+    print rocks.list_network()
