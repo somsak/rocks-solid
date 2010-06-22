97340fa10f16e8ee2037cd58ed8a4c6874eba457
somsaks <somsaks@9156dc64-4839-0410-bff5-3164207c4f9f>
2010-06-22 18:07:36 +0000

    Finish "rocks run ipmi"

--- rocks_solid/rocks/ipmi/__init__.py
+++ rocks_solid/rocks/ipmi/__init__.py
@@ -1,89 +1,6 @@
-# $Id: __init__.py,v 1.8 2009/07/13 19:34:31 bruno Exp $
 #
-# @Copyright@
-# 
-#                 Rocks(r)
-#                  www.rocksclusters.org
-#                version 5.2 (Chimichanga)
-# 
-# Copyright (c) 2000 - 2009 The Regents of the University of California.
-# All rights reserved.    
-# 
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-# 
-# 1. Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-# 
-# 2. Redistributions in binary form must reproduce the above copyright
-# notice unmodified and in its entirety, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided 
-# with the distribution.
-# 
-# 3. All advertising and press materials, printed or electronic, mentioning
-# features or use of this software must display the following acknowledgement: 
-# 
-#     "This product includes software developed by the Rocks(r)
-#     Cluster Group at the San Diego Supercomputer Center at the
-#     University of California, San Diego and its contributors."
-# 
-# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
-# neither the name or logo of this software nor the names of its
-# authors may be used to endorse or promote products derived from this
-# software without specific prior written permission.  The name of the
-# software includes the following terms, and any derivatives thereof:
-# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
-# the associated name, interested parties should contact Technology 
-# Transfer & Intellectual Property Services, University of California, 
-# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
-# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
-# 
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
-# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# 
-# @Copyright@
+# Handle IPMI command
 #
-# $Log: __init__.py,v $
-# Revision 1.8  2009/07/13 19:34:31  bruno
-# fix 'managed' flag
-#
-# Revision 1.7  2009/06/03 18:53:43  mjk
-# - sudo support for ubuntu boy (this is cool)
-# - connect to DB over the network socket not the UNIX domain socket
-# - added x11 param to rocks.run.host to disable x11forwarding
-#
-# Revision 1.6  2009/05/27 20:15:28  bruno
-# add 'managed' flag
-#
-# Revision 1.5  2009/05/01 19:07:02  mjk
-# chimi con queso
-#
-# Revision 1.4  2008/10/18 00:55:57  mjk
-# copyright 5.1
-#
-# Revision 1.3  2008/04/14 21:15:06  bruno
-# let users use 'rocks run host'
-#
-# Revision 1.2  2008/03/06 23:41:39  mjk
-# copyright storm on
-#
-# Revision 1.1  2008/01/29 22:13:05  bruno
-# added 'rocks run host'
-#
-# it executes a command on all listed hosts
-#
-#
-
 import os
 import rocks.commands
 
@@ -92,6 +9,12 @@ from rocks.commands.run.host import RocksRemoteCollator
 import lekatnet.config as config
 import lekatnet.remote as remote
 
+from rocks_solid import config_read
+import rocks_solid.ipmi
+
+rs_config = config_read()
+
+rs_config.power_ignore_host = []
     
 class command(rocks.commands.HostArgumentProcessor,
     rocks.commands.run.command):
@@ -101,7 +24,7 @@ class command(rocks.commands.HostArgumentProcessor,
     
 class Command(command):
     """
-    Run a command for each specified host.
+    Run IPMI command for each specified host.
 
     <arg optional='1' type='string' name='host' repeat='1'>
     Zero, one or more host names. If no host names are supplied, the command
@@ -131,6 +54,8 @@ class Command(command):
     """
 
     def run(self, params, args):
+        ipmi = rocks_solid.ipmi.IPMI(rs_config)
+
         (args, command) = self.fillPositionalArgs(('command', ))
 
         if not command:
@@ -147,20 +72,22 @@ class Command(command):
         conf.load(f)
         f.close()
 
-        dests = RocksRemoteCollator(self)
-        dests.format = '%o\\n'
+        #print '** command ** = ', command
+        ipmi.cmd_all(hosts, command)
 
-        params = conf.getGroupParams('default')
+        #dests = RocksRemoteCollator(self)
+        #dests.format = '%o\\n'
 
-        print '*** param ***'
-        print params
+        #params = conf.getGroupParams('default')
+        # force method
+        #params['method'] = 'localcmd'
 
-        for host in hosts:
-            dests.add(remote.remoteCommandFactory(host, params))
+        #print hosts
 
-        for obj in dests.remoteobjects :
-            print obj
-        #dests.execAll(command)
+        #for host in hosts:
+        #    dests.add(remote.remoteCommandFactory(host, params))
+
+        #dests.execAll('echo ipmitool -I lan -H %(host)s -U admin -E ' + command)
 
         #self.beginOutput()
         #dests.displayAll()
